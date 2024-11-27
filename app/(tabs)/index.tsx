3845acc9597042b16fcf20f0ac2ef52af5577871
Sonny Box <sonny@sonnybox.com>
2024-11-27 11:01:50 -0800

    im tired

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Stack } from 'expo-router'
+import { Link, Stack } from 'expo-router'
 import Header from '../components/header'
 import { View, Text, Platform } from 'react-native'
 
 export default function Tab() {
-    const buttons = [
+    type Button = {
+        label: string
+        icon: string
+        link: any
+    }
+    const buttons: Button[] = [
         { label: 'Sleep', icon: '🌙', link: '/sleep' },
         { label: 'Pump', icon: '🍼', link: '/pump' },
         { label: 'Milestone', icon: '🌟', link: '/milestone' },
@@ -21,24 +26,25 @@ export default function Tab() {
             />
             <View className='flex-row flex-wrap h-full justify-center align-center pt-20 gap-4 bg-white'>
                 {buttons.map((button, key) => (
-                    <View
-                        className='border-4 w-52 h-52 rounded-[40] items-center justify-center bg-white' // items-center is x, justify-center is y
-                        key={key}
-                    >
-                        <Text className='text-7xl p-4'>{button.icon}</Text>
-                        <Text
-                            className='text-black'
-                            style={{
-                                fontFamily: Platform.select({
-                                    android: 'Figtree_700ExtraBold',
-                                    ios: 'Figtree-ExtraBold',
-                                }),
-                                fontSize: 18,
-                            }}
+                    <Link href={button.link} key={key}>
+                        <View
+                            className='border-4 w-52 h-52 rounded-[40] items-center justify-center bg-white' // items-center is x, justify-center is y
                         >
-                            {button.label}
-                        </Text>
-                    </View>
+                            <Text className='text-7xl p-4'>{button.icon}</Text>
+                            <Text
+                                className='text-black'
+                                style={{
+                                    fontFamily: Platform.select({
+                                        android: 'Figtree_700ExtraBold',
+                                        ios: 'Figtree-ExtraBold',
+                                    }),
+                                    fontSize: 18,
+                                }}
+                            >
+                                {button.label}
+                            </Text>
+                        </View>
+                    </Link>
                 ))}
             </View>
         </>
